cb332a36748ed387d32dfe18dd8093b94a928fdf
Tran Duong <duong.tran@invincix.com>
2024-02-18 17:46:17 +0700

    fix gallery responsive

--- app/gallery/Gallery.tsx
+++ app/gallery/Gallery.tsx
@@ -2,6 +2,13 @@
 
 import gsap from 'gsap'
 import { createElement, useRef } from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
 
 export default function Gallery() {
   const preview = useRef<HTMLDivElement>(null)
@@ -21,41 +28,65 @@ export default function Gallery() {
   }
 
   return (
-    <div className='container flex justify-between mt-8'>
-      <div className='grid grid-cols-1 lg:grid-cols-4 gap-8 max-h-[600px] overflow-y-scroll'>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
-          <div
-            key={item}
-            className='flex items-center flex-col justify-center cursor-pointer'
-            onClick={() =>
-              handleImageClick(`/images/gallery/gallery-${item}.jpg`)
-            }
-          >
-            <p>{item}</p>
+    <>
+      <div className='hidden container lg:flex justify-between mt-8'>
+        <div className='grid grid-cols-1 lg:grid-cols-4 gap-8 max-h-[600px] overflow-y-scroll'>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
             <div
-              className='w-[150px] h-[150px]'
-              style={{
-                backgroundImage: `url('/images/gallery/gallery-${item}.jpg')`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-              }}
-            ></div>
-          </div>
-        ))}
-      </div>
-      <div
-        ref={previewContainer}
-        className='relative w-[250px] lg:w-[500px] lg:h-[600px] overflow-hidden'
-      >
+              key={item}
+              className='flex items-center flex-col justify-center cursor-pointer'
+              onClick={() =>
+                handleImageClick(`/images/gallery/gallery-${item}.jpg`)
+              }
+            >
+              <p>{item}</p>
+              <div
+                className='w-[150px] h-[150px]'
+                style={{
+                  backgroundImage: `url('/images/gallery/gallery-${item}.jpg')`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center center',
+                }}
+              ></div>
+            </div>
+          ))}
+        </div>
         <div
-          className='preview w-[250px] lg:w-[500px] lg:h-[600px] absolute'
-          style={{
-            backgroundImage: `url('/images/gallery/gallery-1.jpg')`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center center',
-          }}
-        ></div>
+          ref={previewContainer}
+          className='relative w-[250px] lg:w-[500px] lg:h-[600px] overflow-hidden'
+        >
+          <div
+            className='preview w-[250px] lg:w-[500px] lg:h-[600px] absolute'
+            style={{
+              backgroundImage: `url('/images/gallery/gallery-1.jpg')`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center center',
+            }}
+          ></div>
+        </div>
       </div>
-    </div>
+      <div className='container w-full block lg:hidden mt-8'>
+        <Carousel>
+          <CarouselContent>
+            {Array.from({ length: 12 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div
+                  className='w-full h-[350px]'
+                  style={{
+                    backgroundImage: `url('/images/gallery/gallery-${
+                      index + 1
+                    }.jpg')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                  }}
+                ></div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </>
   )
 }
