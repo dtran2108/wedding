2ade0a04e12e33b5e321aee6799fd92730cca119
Tran Duong <duong.tran@invincix.com>
2024-02-19 14:55:20 +0700

    add anouncement section

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,55 @@
-'use client'
-
-import background from '@/public/images/background.jpg'
-import { WindSong } from 'next/font/google'
+import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
-import AnimatedTextWord from '@/components/AnimatedTextWord'
+import { Playfair, Playfair_Display } from 'next/font/google'
+import Image from 'next/image'
 
-const windSong = WindSong({ subsets: ['latin'], weight: '400' })
+const playfair = Playfair_Display({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <div
-        className='absolute -top-0 min-h-screen w-screen flex justify-center'
-        style={{
-          backgroundImage: `url(${background.src})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center center',
-        }}
-      >
-        <div className='flex flex-col mt-40'>
-          <AnimatedTextWord text="WE'RE GETTING MARRIED!" />
-          <AnimatedTextWord
-            className={cn(windSong.className, 'text-[54px] lg:text-[76px]')}
-            text='Duy + Điền'
-          />
-          <div className='flex items-center justify-end'>
-            <AnimatedTextWord text='20.04.2024' />
-          </div>
+    <main className='min-h-screen container mt-24'>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+        <div>
+          <h2 className={cn(playfair.className, 'text-5xl leading-normal')}>
+            20.04.2024
+          </h2>
+          <h2 className={cn(playfair.className, 'text-5xl leading-normal')}>
+            We're getting married!
+          </h2>
+          <h1 className={cn(playfair.className, 'text-7xl leading-normal')}>
+            Quân - Hiền
+          </h1>
+          <p className='text-[#baad9e] mt-4 w-9/12'>
+            Get ready to witness a love story take center stage as two hearts
+            become one in a joyous celebration of forever!
+          </p>
+          <Button className='mt-8'>RSVP</Button>
+        </div>
+        <div className='relative hidden lg:block'>
+          <div
+            className='w-[300px] h-[120%] mx-auto absolute -top-12 left-24 z-10'
+            style={{
+              backgroundImage: `url('/images/cover-3.jpeg')`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center center',
+            }}
+          ></div>
+          <div
+            className='w-[200px] h-[150px] mx-auto absolute left-[50%] z-9'
+            style={{
+              backgroundImage: `url('/images/cover-2.jpeg')`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center center',
+            }}
+          ></div>
+          <div
+            className='w-[150px] h-[200px] mx-auto absolute bottom-0 z-20 rounded-t-full'
+            style={{
+              backgroundImage: `url('/images/cover-1.jpeg')`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center center',
+            }}
+          ></div>
         </div>
       </div>
     </main>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,98 +1,29 @@
 'use client'
 
-import { motion } from 'framer-motion'
-import Link from './Link'
-import { usePathname } from 'next/navigation'
-import Menu from '@/icons/Menu'
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
-
-const menus = [
-  {
-    title: 'Our Story',
-    link: '/our-story',
-  },
-  {
-    title: 'Gallery',
-    link: '/gallery',
-  },
-  {
-    title: 'Schedule',
-    link: '/schedule',
-  },
-  {
-    title: 'Details',
-    link: '/details',
-  },
-  {
-    title: 'Travel Info',
-    link: '/travel-info',
-  },
-]
+import { cn } from '@/lib/utils'
+import { Bebas_Neue } from 'next/font/google'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { Diamond } from 'lucide-react'
 
 export default function NavBar() {
-  const pathname = usePathname()
-
   return (
-    <nav className='sticky top-0 bg-white backdrop-filter backdrop-blur-sm bg-opacity-60 border-b border-gray-200 firefox:bg-opacity-60 z-50'>
-      <div className='max-w-5xl mx-auto px-4'>
-        <div className='flex items-center justify-between h-16'>
-          <motion.a
-            whileHover={{
-              color: '#E6C994',
-            }}
-            className='text-2xl text-gray-900 font-semibold'
-            href='/'
-          >
-            D&Đ
-          </motion.a>
-          <div className='hidden lg:flex space-x-3 lg:space-x-6 text-gray-900'>
-            {menus.map((item) => (
-              <Link
-                key={item.title}
-                title={item.title}
-                href={item.link}
-                isActive={pathname == item.link}
-              />
-            ))}
-          </div>
-          <Sheet>
-            <SheetTrigger className='block lg:hidden'>
-              <Menu className='cursor-pointer' />
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>
-                  <motion.a
-                    whileHover={{
-                      color: '#E6C994',
-                    }}
-                    className='text-2xl text-gray-900 font-semibold'
-                    href='/'
-                  >
-                    D&Đ
-                  </motion.a>
-                </SheetTitle>
-              </SheetHeader>
-              <div className='py-4'>
-                {menus.map((item) => (
-                  <div key={item.title}>
-                    <Link
-                      title={item.title}
-                      href={item.link}
-                      isActive={pathname == item.link}
-                    />
-                  </div>
-                ))}
-              </div>
-            </SheetContent>
-          </Sheet>
+    <nav className='flex items-center justify-between py-4'>
+      <Link href='/'>
+        <div className={cn('text-center text-2xl font-bold flex items-center')}>
+          <Diamond className='w-4 h-4 mr-2' /> Q&H
         </div>
+      </Link>
+      <div className='flex items-center space-x-12'>
+        <Link href='/our-story'>Our Story</Link>
+        <Link href='/gallery'>Gallery</Link>
+        <Link href='/schedule'>Schedule</Link>
+        <Link href='/details'>Details</Link>
+      </div>
+      <div>
+        <Button variant='outline' className='font-bold text-primary'>
+          RSVP
+        </Button>
       </div>
     </nav>
   )
