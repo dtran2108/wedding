88bd0f0833c199e9a7af7a224324e2ac95307a40
Tran Duong <duong.tran@invincix.com>
2024-02-21 01:05:33 +0700

    make scrollbar responsive

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -14,53 +14,53 @@ export default function NavBar() {
   }
 
   return (
-    <nav className='flex items-center justify-evenly bg-black/70 py-3 sticky top-0 text-[#b2b2b2] z-30 text-sm'>
+    <nav className='flex px-8 items-center space-x-16 justify-start md:justify-center bg-black/70 py-3 sticky top-0 text-[#b2b2b2] z-30 text-sm overflow-x-scroll w-full no-scrollbar'>
       <motion.span
         onClick={() => scrollToHash('our-story')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         OUR STORY
       </motion.span>
       <motion.span
         onClick={() => scrollToHash('gallery')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         GALLERY
       </motion.span>
       <motion.span
         onClick={() => scrollToHash('bridal-party')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         BRIDAL PARTY
       </motion.span>
       <motion.span
         onClick={() => scrollToHash('festivities')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         FESTIVITIES
       </motion.span>
       <motion.span
         onClick={() => scrollToHash('registry')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         TRAVEL
       </motion.span>
       <motion.span
         onClick={() => scrollToHash('to-do')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         TO DO/TO EAT
       </motion.span>
       <motion.span
         onClick={() => scrollToHash('add-to-our-story')}
         whileHover={{ scale: 1.1 }}
-        className='cursor-pointer'
+        className='cursor-pointer whitespace-nowrap'
       >
         ADD TO OUR STORY
       </motion.span>
