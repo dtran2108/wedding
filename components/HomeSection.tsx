28ba12ebb25f293e9dffc0e322bbe66a039dd00d
Tran Duong <duong.tran@invincix.com>
2024-03-09 00:57:11 +0800

    add dress code dialog

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -2,10 +2,11 @@
 
 import { cn } from '@/lib/utils'
 import { Water_Brush } from 'next/font/google'
-import Image from 'next/image'
 import Divider from './Divider'
 import { useEffect, useState } from 'react'
 import RSVPButton from './RSVPButton'
+import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
+import DressCode from './DressCode'
 
 const waterBrush = Water_Brush({ subsets: ['latin'], weight: '400' })
 
@@ -213,29 +214,36 @@ export default function HomeSection() {
           }}
         ></div>
 
-        <div
-          className={cn(
-            mainStyle,
-            'col-span-1 row-span-1 p-4 items-center justify-center space-y-6 flex-col bg-white cursor-pointer',
-            'hidden lg:flex'
-          )}
-          onClick={() => scrollToHash('bridesmaids')}
-        >
-          <div className='flex items-center justify-center space-x-4'>
-            <span className='w-[40px] aspect-square rounded-full bg-[#ffecf5]'></span>
-            <span className='w-[40px] aspect-square rounded-full bg-[#ffe7e9]'></span>
-            <span className='w-[40px] aspect-square rounded-full bg-[#fdcee0]'></span>
-            <span className='w-[40px] aspect-square rounded-full bg-[#f2bfd2]'></span>
-          </div>
-          <h1
-            className={cn(
-              waterBrush.className,
-              'text-primary font-bold text-3xl text-center'
-            )}
-          >
-            DRESS CODE
-          </h1>
-        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <div
+              className={cn(
+                mainStyle,
+                'col-span-1 row-span-1 p-4 items-center justify-center space-y-6 flex-col bg-white cursor-pointer',
+                'hidden lg:flex'
+              )}
+              onClick={() => scrollToHash('bridesmaids')}
+            >
+              <div className='flex items-center justify-center space-x-4'>
+                <span className='w-[40px] aspect-square rounded-full bg-[#ffecf5]'></span>
+                <span className='w-[40px] aspect-square rounded-full bg-[#ffe7e9]'></span>
+                <span className='w-[40px] aspect-square rounded-full bg-[#fdcee0]'></span>
+                <span className='w-[40px] aspect-square rounded-full bg-[#f2bfd2]'></span>
+              </div>
+              <h1
+                className={cn(
+                  waterBrush.className,
+                  'text-primary font-bold text-3xl text-center'
+                )}
+              >
+                DRESS CODE
+              </h1>
+            </div>
+          </DialogTrigger>
+          <DialogContent className='max-h-[500px] overflow-y-scroll'>
+            <DressCode />
+          </DialogContent>
+        </Dialog>
 
         <div
           onClick={() => scrollToHash('contact')}
